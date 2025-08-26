49dee7d04db2f4fb731380c6501d4993f4599051
Niranjan Kushwaha <0niranjankumar1@gmail.com>
2025-08-26 21:39:06 +0530

    environment setup done

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
@@ -6,14 +6,23 @@ import { dirname, resolve } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 4001,
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    server: {
+      port: 4001,
     },
-  },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
+    },
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV || mode),
+    },
+  };
 });
